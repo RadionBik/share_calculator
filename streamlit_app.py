df90c2c341fe3435310fffb2e73dfec143348a3e
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2021-11-04 21:33:46 +0300

    print equation, add explicit calculator

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,40 +2,66 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+P1_SHARE = 50.0
+P2_SHARE = 10.0
 
-def calc_share(profit_thousands, p1, p2):
-    p1_share = 50
-    p2_share = 10
-    if profit_thousands <= p1:
-        return p1_share
-    elif profit_thousands >= p2:
-        return p2_share
-    else:
-        coeff_matrix = np.array([[1, p1], [1, p2]])
-        dep_var = np.array([p1_share, p2_share])
-        b, k = np.linalg.solve(coeff_matrix, dep_var)
-        return k * profit_thousands + b
+
+def solve_linear_equation(p1, p2):
+    coeff_matrix = np.array([[1, p1], [1, p2]])
+    dep_var = np.array([P1_SHARE, P2_SHARE])
+    b, k = np.linalg.solve(coeff_matrix, dep_var)
+    return k, b
+
+
+class ShareCalculator:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        k, b = solve_linear_equation(p1, p2)
+        self.k = k
+        self.b = b
+
+    def calc_share(self, profit_thousands) -> float:
+        if profit_thousands <= self.p1:
+            return P1_SHARE
+        elif profit_thousands >= self.p2:
+            return P2_SHARE
+        else:
+            return self.k * profit_thousands + self.b
+
+    def print_equation(self) -> str:
+        return """
+        Формула расчета:
+        
+        если ПРИБЫЛЬ <= {}, то S={}
+        если ПРИБЫЛЬ >= {}, то S={}
+        иначе S={:2.4f} * ПРИБЫЛЬ + {:2.4f}
+        """.format(self.p1, P1_SHARE, self.p2, P2_SHARE, self.k, self.b)
 
 
 def main():
     st.markdown(
         """
-        Формула доли S считается для диапазона (P1, P2),
-        где P1 -- верхний порог прибыли, для которой доля `S=50%`,
-        а P2 -- нижний порог прибыли, для которой доля `S=10%`.
+        Формула доли S считается для диапазона `(П1, П2)`,
+        где `П1` -- верхний порог прибыли, для которой доля `S=50%`,
+        а `П2` -- нижний порог прибыли, для которой доля `S=10%`.
         
         Внутри диапазона используется линейное уравнение вида 
-        `S = k * прибыль + b`
+        `S = k * ПРИБЫЛЬ + b`, которое считается автоматически.
         """
     )
-    p1 = st.slider('введи P1', value=100, min_value=50, max_value=150)
-    p2 = st.slider('введи P2', value=500, min_value=200, max_value=1000)
-
+    p1 = st.slider('Выбери П1, тысяч рублей:', value=200, min_value=100, max_value=300)
+    p2 = st.slider('Выбери П2, тысяч рублей:', value=500, min_value=200, max_value=800)
+    calculator = ShareCalculator(p1, p2)
+    st.text(calculator.print_equation())
     money_range = range(0, 1000)
     data = pd.DataFrame({
-        'Доля S, %': list(map(lambda x: calc_share(x, p1, p2), money_range)),
+        'Доля S, %': map(lambda x: calculator.calc_share(x), money_range),
     })
     st.line_chart(data)
+    profit = st.number_input('Введи общую прибыль, тысяч рублей:', value=250.0)
+    share = calculator.calc_share(profit)
+    st.text('Для общей прибыли в {} тыс. рублей S={:2.2f}%'.format(profit, share))
 
 
 if __name__ == '__main__':
