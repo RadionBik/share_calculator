731303552c4d4d6a78342596ef5de6c8902cdd65
Radion Bikmukhamedov <radion.bikmukhamedov@pm.me>
2021-11-04 23:16:28 +0300

    revisit formula

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,67 +1,63 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
 
-P1_SHARE = 50.0
-P2_SHARE = 10.0
-
-
-def solve_linear_equation(p1, p2):
-    coeff_matrix = np.array([[1, p1], [1, p2]])
-    dep_var = np.array([P1_SHARE, P2_SHARE])
-    b, k = np.linalg.solve(coeff_matrix, dep_var)
-    return k, b
+MONEY_MEASURE = '[тыс. руб]'
+START_SHARE = 0.5
 
 
 class ShareCalculator:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-        k, b = solve_linear_equation(p1, p2)
-        self.k = k
-        self.b = b
+    def __init__(self, p, remainder_share_percent):
+        self.p = p
+        self.remainder_share_percent = remainder_share_percent
 
-    def calc_share(self, profit_thousands) -> float:
-        if profit_thousands <= self.p1:
-            return P1_SHARE
-        elif profit_thousands >= self.p2:
-            return P2_SHARE
+    def calc_share_percent(self, profit_thousands) -> float:
+        other_share = self.calc_other_share(profit_thousands)
+        return other_share * 100 / profit_thousands
+
+    def calc_other_share(self, profit, ) -> float:
+        if profit <= self.p:
+            return profit * START_SHARE
         else:
-            return self.k * profit_thousands + self.b
+            return (self.p * START_SHARE) + (profit - self.p) * self.remainder_share_percent / 100
 
     def print_equation(self) -> str:
         return """
         Формула расчета:
-        
-        если ПРИБЫЛЬ <= {}, то S={}
-        если ПРИБЫЛЬ >= {}, то S={}
-        иначе S={:2.4f} * ПРИБЫЛЬ + {:2.4f}
-        """.format(self.p1, P1_SHARE, self.p2, P2_SHARE, self.k, self.b)
+
+        если ПРИБЫЛЬ <= П, то S = 50 [%] * ПРИБЫЛЬ,
+        иначе S = ({start_share} * {p} {money_measure}) + (ПРИБЫЛЬ {money_measure} - {p} {money_measure}) * {share_percent}
+        """.format(start_share=START_SHARE, p=self.p, share_percent=self.remainder_share_percent / 100,
+                   money_measure=MONEY_MEASURE)
 
 
 def main():
     st.markdown(
         """
-        Формула доли S считается для диапазона `(П1, П2)`,
-        где `П1` -- верхний порог прибыли, для которой доля `S=50%`,
-        а `П2` -- нижний порог прибыли, для которой доля `S=10%`.
-        
-        Внутри диапазона используется линейное уравнение вида 
-        `S = k * ПРИБЫЛЬ + b`, которое считается автоматически.
-        """
+        Формула расчета доли `S` задается двумя параметрами:
+        * `П {money_measure}` - порог прибыли, для которого доля равна `50%`,
+        * `R [%]` - процент на разницу между общей `ПРИБЫЛЬЮ` и `П`.
+
+        """.format(money_measure=MONEY_MEASURE)
     )
-    p1 = st.slider('Выбери П1, тысяч рублей:', value=200, min_value=100, max_value=300)
-    p2 = st.slider('Выбери П2, тысяч рублей:', value=500, min_value=200, max_value=800)
-    calculator = ShareCalculator(p1, p2)
+
+    p = st.slider(f'Выбери П {MONEY_MEASURE}:', value=200, min_value=100, max_value=300)
+    remainder_share_percent = st.slider(f'Выбери долю на остаток R [%]:', value=15, min_value=5, max_value=30)
+    calculator = ShareCalculator(p, remainder_share_percent)
     st.text(calculator.print_equation())
-    money_range = range(0, 1000)
-    data = pd.DataFrame({
-        'Доля S, %': map(lambda x: calculator.calc_share(x), money_range),
-    })
-    st.line_chart(data)
-    profit = st.number_input('Введи общую прибыль, тысяч рублей:', value=250.0)
-    share = calculator.calc_share(profit)
-    st.text('Для общей прибыли в {} тыс. рублей S={:2.2f}%'.format(profit, share))
+    money_range = range(1, 500)
+    st.line_chart(pd.DataFrame({
+        'Доля S [%]': map(lambda x: calculator.calc_share_percent(x), money_range),
+        f'Доля S {MONEY_MEASURE}': map(lambda x: calculator.calc_other_share(x), money_range),
+
+    }))
+    profit = st.number_input(f'Введи общую ПРИБЫЛЬ {MONEY_MEASURE}:', value=250.0)
+    share_percent = calculator.calc_share_percent(profit)
+    share = calculator.calc_other_share(profit)
+    st.text('Для общей ПРИБЫЛИ = {} {money_measure}:\n'
+            'S [%] = {:2.2f}\n'
+            'S {money_measure} = ПРИБЫЛЬ {money_measure} * S [%] = {:4.2f}'.format(
+        profit, share_percent, share, money_measure=MONEY_MEASURE
+    ))
 
 
 if __name__ == '__main__':
